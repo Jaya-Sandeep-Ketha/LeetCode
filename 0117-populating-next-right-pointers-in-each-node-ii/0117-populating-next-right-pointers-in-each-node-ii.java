451b483101930be9991caa7813470bb2bce5acc0
Jaya-Sandeep-Ketha <140141084+Jaya-Sandeep-Ketha@users.noreply.github.com>
2023-10-29 07:20:28 -0400

    Time: 1 ms (74.68%), Space: 43.9 MB (7.57%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
@@ -21,28 +21,56 @@ class Node {
 };
 */
 
+
 class Solution {
     public Node connect(Node root) {
         if(root==null){
             return null;
         }
-        Node head = root;
-        while(head!=null){
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
             Node dummy = new Node(0);
-            Node temp = dummy;
-            while(head!=null){
-                if(head.left!=null){
-                    temp.next = head.left;
-                    temp = temp.next;
+            while(size-->0){
+                Node node = q.remove();
+                dummy.next = node;
+                dummy = dummy.next;
+                if(node.left!=null){
+                    q.add(node.left);
                 }
-                if(head.right!=null){
-                    temp.next = head.right;
-                    temp = temp.next;
+                if(node.right!=null){
+                    q.add(node.right);
                 }
-                head = head.next;
             }
-            head = dummy.next;
         }
         return root;
     }
 }
+
+
+// class Solution {
+//     public Node connect(Node root) {
+//         if(root==null){
+//             return null;
+//         }
+//         Node head = root;
+//         while(head!=null){
+//             Node dummy = new Node(0);
+//             Node temp = dummy;
+//             while(head!=null){
+//                 if(head.left!=null){
+//                     temp.next = head.left;
+//                     temp = temp.next;
+//                 }
+//                 if(head.right!=null){
+//                     temp.next = head.right;
+//                     temp = temp.next;
+//                 }
+//                 head = head.next;
+//             }
+//             head = dummy.next;
+//         }
+//         return root;
+//     }
+// }
